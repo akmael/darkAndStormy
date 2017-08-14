2219cc1241af476040edd8fd08955f92bda4ddc9
AndraIshmael <ishmaelak@gmail.com>
2017-08-14 11:24:58 +0100

    framed frontend

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const favicon = require('serve-favicon')
 const server = express();
 
 const port = process.env.PORT || 8080;
 
-
+server.use(favicon(path.join(__dirname, 'public', 'media', 'favicon.ico')))
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', function(req,res){
